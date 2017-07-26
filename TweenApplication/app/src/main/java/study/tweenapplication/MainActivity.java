150b288d78d32494a881395507274fbed0d48d30
sungju <lsj0147@naver.com>
2017-07-26 15:43:17 +0900

    TweenAnimation

---
+++ TweenApplication/app/src/main/java/study/tweenapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package study.tweenapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 첫번째 버튼 이벤트 처리
+        Button scaleButton = (Button) findViewById(R.id.scaleButton);
+        scaleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale);
+                v.startAnimation(anim);
+            }
+        });
+
+        // 두번째 버튼 이벤트 처리
+        Button scale2Button = (Button) findViewById(R.id.scale2Button);
+        scale2Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale2);
+                v.startAnimation(anim);
+            }
+        });
+
+    }
+}
